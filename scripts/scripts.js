64f1c2707d1e4891858724918675a0213103e44e
JulSarmiento <jasa1999@hotmail.com>
2021-08-17 15:54:38 -0500

    añadiehndo componentes de bootstrap, header, slider

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+// Modasl for Fomrs
+
+let myModal = document.getElementById('homeForm')
+let myInput = document.getElementById('homeFormModal')
+
+homeForm.addEventListener('shown.bs.modal', function modasl() {
+  homeFormModal.focus()
+})
