2e23aac334d7d408c6b945d1dc7ae63016cbb1c5
JulSarmiento <jasa1999@hotmail.com>
2021-08-25 18:03:54 -0500

    Se paso el codigo de css a sass

--- scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-// Modasl for Fomrs
-
-let myModal = document.getElementById('homeForm')
-let myInput = document.getElementById('homeFormModal')
-
-homeForm.addEventListener('shown.bs.modal', function modasl() {
-  homeFormModal.focus()
-})
